d243352c9bdda052cd2d12e46ad0132624530713
ericlnewman <eric.leon.newman@gmail.com>
2023-11-17 20:15:44 -0500

    Updated the javascript to include a function that returns a randonmly generated console log of rock, paper or scissors. The logic is to use the Math.random within the numbers 1-4, and an object that has the keys of rock, paper or scissors and then using Object,keys and the find() method, return the key based on the randomly generated number; with assigning paper to 2 and 4 as these appear less than 1 and 3 during testing.

--- script/script.js
+++ script/script.js
@@ -1,1 +1,19 @@
 /*JavaScript for Rock Paper Scissors Odin Project Assignment*/
+
+// function for single player in the console, with computer making choices:
+//  it will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+function getComputerChoice(){
+    // create a number between 1 and 4 
+    let number = Math.floor(Math.random()*4)+1;
+    // assign a number for each of the possible choices
+    const obj = {
+        rock : 1,
+        paper : 2,
+        scissors : 3,
+        paper : 4
+    };
+    // return the keys based on their given values that correspond to the randomly generated number:
+    return Object.keys(obj).find(key => 
+        obj[key] === number);
+}
+console.log(getComputerChoice());
