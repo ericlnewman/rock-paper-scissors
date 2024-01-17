719fed266765bcd6574f7ffadf528b6a894ed37c
ericlnewman <eric.leon.newman@gmail.com>
2024-01-17 15:25:42 -0500

    updated index with a div to contain the updated ui interface, along with implementing buttons that run via the DOM and interact as before, with showing information on the console. The results of the 5 rounds shows if the player won or lost on a dynamically created div.

--- script/script.js
+++ script/script.js
@@ -1,5 +1,56 @@
 /*JavaScript for Rock Paper Scissors Odin Project Assignment*/
 
+
+// change: added UI buttons that are placed in newly added div within HTML
+const container = document.querySelector("#container");
+const h3Header = document.createElement("h3");
+const h4Header = document.createElement("h4");
+const rockBtn = document.createElement("button");
+const scissorsBtn = document.createElement("button");
+const paperBtn = document.createElement("button");
+
+
+h3Header.textContent = "Rock, Paper, Scissors!"
+h4Header.textContent = "Play some rounds against the computer in the console! Can you win????? (press f12 to see the console)"
+rockBtn.textContent = "Rock";
+paperBtn.textContent = "Paper";
+scissorsBtn.textContent = "Scissors";
+
+
+rockBtn.id = "rockBtn";
+scissorsBtn.id = "scissorsBtn";
+paperBtn.id = "paperBtn";
+
+container.appendChild(h3Header);
+container.appendChild(h4Header);
+container.appendChild(rockBtn);
+container.appendChild(scissorsBtn);
+container.appendChild(paperBtn);
+
+
+// added: function to handle the different outcomes when a button is pressed
+function getUserChoice(target){
+    switch(target){
+        case "rockBtn":
+            return "rock";
+        case "scissorsBtn":
+            return "scissors";
+        case "paperBtn":
+            return "paper";
+        default:
+            return "";
+    }
+}
+// added: promise to pause the for loop and wait user input
+function waitForUserInput(){
+    return new Promise((resolve) =>{
+        container.addEventListener("click", function handleClick(e){
+            const userChoice = getUserChoice(e.target.id);
+            resolve(userChoice);
+            container.removeEventListener("click", handleClick);
+        });
+    });
+}
 // function for single player in the console, with computer making choices:
 //  it will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
 function getComputerChoice(){
@@ -16,7 +67,6 @@ function getComputerChoice(){
     return Object.keys(obj).find(key => 
         obj[key] === number);
 }
-// console.log(getComputerChoice());
 /*
 function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection -
 and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
@@ -25,31 +75,42 @@ recall:
 - paper beats rock
 - scissors beats paper
 */
-function playRound(playerSelection, computerSelection, tally){
-    const p = playerSelection.toLowerCase();
+function playRound(playerSelection, computerSelection){
+    // change: made computerSelection be assigned the getComputerChoice, without having to call it in other functions
+    if(computerSelection === undefined){
+        computerSelection = getComputerChoice();
+    }
+    const p = playerSelection;
     const c = computerSelection;
     if(p === c || c == undefined){return "It's a tie!";}
     else if(p === "rock" && c === "paper") {return "You Lose! Paper beats Rock";}
-    else if(p === "rock" && c === "scissors") {tally--; return "You Win! Rock beats Scissors";}
+    else if(p === "rock" && c === "scissors") {return "You Win! Rock beats Scissors";}
     else if(p === "paper" && c === "scissors") {return "You Lose! Scissors beats Paper";}
-    else if(p === "paper" && c === "rock") {tally--; return "You Win! Paper beats Rock";}
+    else if(p === "paper" && c === "rock") {return "You Win! Paper beats Rock";}
     else if(p === "scissors" && c === "rock") {return "You Lose! Rock beats Scissors";}
-    else if (p === "scissors" && c === "paper") {tally--; return "You Win! Scissors beats Paper";}
+    else if(p === "scissors" && c === "paper") {return "You Win! Scissors beats Paper";}
 }
-/*
-console.log(playRound("RoCK", getComputerChoice()));
-console.log(playRound("paPer", getComputerChoice()));
-console.log(playRound("Scissors", getComputerChoice()));
-console.log(playRound("sciSSORs", getComputerChoice()));
-*/
-function game(){
+
+// changed: game function into an async/await that uses the waitForUserInput promise to wait for button clicks
+async function game(){
     console.log("Welcome to Rock, Paper, Scissors! Play 5 rounds against the computer! Can you win?????");
     let tally = 5;
     for(let i = 0; i < 5; i++){
-        const userInput = prompt("Rock, paper or scissors, which do you choose?");
-        console.log(playRound(userInput, getComputerChoice(), tally));
+        const userInput = await waitForUserInput();
+        const currentRound = playRound(userInput);
+        console.log(currentRound);
+        // changed means to decrement tally to determiner winner
+        if(currentRound.includes("You Win!")){
+            tally--;
+        }
     }
-    if(tally < 3) return "You won!";
-    else return "You lost!";
+    let resultOfRounds;
+    const resultsDiv = document.createElement("div");
+    const spanResult = document.createElement("span");
+    if(tally < 3){resultOfRounds = "You won!";}
+    else {resultOfRounds = "You lost!";}
+    spanResult.textContent = resultOfRounds;
+    resultsDiv.appendChild(spanResult);
+    container.appendChild(resultsDiv);
 }
 console.log(game());
